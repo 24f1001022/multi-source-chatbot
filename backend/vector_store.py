14a21aea089fae0fc9b7ca95e00b14b595dde577
Safwan Humayun <24f1001022@ds.study.iitm.ac.in>
2026-06-17 18:08:30 +0530

    fix: use HuggingFace API embeddings to reduce RAM usage on free tier

--- backend/vector_store.py
+++ backend/vector_store.py
@@ -1,46 +1,29 @@
 from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEndpointEmbeddings
+from config import EMBEDDING_MODEL, VECTOR_DB_PATH, HF_TOKEN
 
-from langchain_huggingface import (
-    HuggingFaceEmbeddings
-)
-
-from config import (
-    EMBEDDING_MODEL,
-    VECTOR_DB_PATH
-)
-
+_embeddings = None
 
 def get_embeddings():
-
-    return HuggingFaceEmbeddings(
-        model_name=EMBEDDING_MODEL
-    )
-
+    global _embeddings
+    if _embeddings is None:
+        _embeddings = HuggingFaceEndpointEmbeddings(
+            model=EMBEDDING_MODEL,
+            huggingfacehub_api_token=HF_TOKEN
+        )
+    return _embeddings
 
 def save_vectorstore(chunks):
-
     embeddings = get_embeddings()
-
-    db = FAISS.from_documents(
-        chunks,
-        embeddings
-    )
-
-    db.save_local(
-        VECTOR_DB_PATH
-    )
-
+    db = FAISS.from_documents(chunks, embeddings)
+    db.save_local(VECTOR_DB_PATH)
     return db
 
-
 def load_vectorstore():
-
     embeddings = get_embeddings()
-
     db = FAISS.load_local(
         VECTOR_DB_PATH,
         embeddings,
         allow_dangerous_deserialization=True
     )
-
     return db
